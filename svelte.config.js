0fd7127ed82ffee8a8aa864a80468b89dbd53a93
edoardo <edoardobalzano01@gmail.com>
2024-03-21 23:24:27 +0100

    Added adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,6 +8,9 @@ const config = {
   preprocess: [vitePreprocess({})],
 
   kit: {
+    adapter: adapter({
+      // see below for options that can be set here
+    })
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
